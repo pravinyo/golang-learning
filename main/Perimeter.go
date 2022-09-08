2c473e88935f793000a2cbcf3383cb5915d2c9cb
Pravin Tripathi <pravin.tripathi@thoughtworks.com>
2022-09-08 17:25:56 +0530

    test improved using table test

--- main/Perimeter.go
+++ main/Perimeter.go
@@ -15,12 +15,21 @@ type Circle struct {
 	radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.width * r.height
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- main/Perimeter_test.go
+++ main/Perimeter_test.go
@@ -12,21 +12,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle area", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "Circle area", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle area", shape: Triangle{12, 6}, want: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("for %q got %g want %g", tt.name, got, tt.want)
+		}
+	}
 }
