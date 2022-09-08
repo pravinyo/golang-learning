ad15a5ae72748bc9172232777e1a58622a6918e6
Pravin Tripathi <pravin.tripathi@thoughtworks.com>
2022-09-08 15:10:58 +0530

    added empty name handling

--- main/hello.go
+++ main/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const helloEnglishPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return helloEnglishPrefix + name
 }
 
--- main/hello_test.go
+++ main/hello_test.go
@@ -5,9 +5,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Pravin")
-	want := "Hello, Pravin"
+	t.Run("Say hello to people", func(t *testing.T) {
+		got := Hello("Pravin")
+		want := "Hello, Pravin"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
