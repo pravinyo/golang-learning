8e45b591d5475b3f40b28f79c11121ff1669e84e
Pravin Tripathi <pravin.tripathi@thoughtworks.com>
2022-09-08 15:27:22 +0530

    refactored to use switch statement

--- main/hello.go
+++ main/hello.go
@@ -7,16 +7,18 @@ const helloHindiPrefix = "नमस्ते, "
 const helloFrenchPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
-	prefix := helloEnglishPrefix
+	var prefix string
 	if name == "" {
 		name = "World"
 	}
-	if language == "Hindi" {
-		prefix = helloHindiPrefix
-	}
 
-	if language == "French" {
+	switch language {
+	case "French":
 		prefix = helloFrenchPrefix
+	case "Hindi":
+		prefix = helloHindiPrefix
+	default:
+		prefix = helloEnglishPrefix
 	}
 
 	return prefix + name
