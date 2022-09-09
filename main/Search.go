ea28ac7a3ce1dbc001f04746beb74965c2c238df
Pravin Tripathi <pravin.tripathi@thoughtworks.com>
2022-09-09 14:40:54 +0530

    add word to dictionary

--- main/Search.go
+++ main/Search.go
@@ -1,5 +1,20 @@
 package main
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(key string) (string, error) {
+	value, ok := d[key]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return value, nil
+}
+
+func (d Dictionary) Add(key string, value string) {
+	d[key] = value
 }
--- main/Search_test.go
+++ main/Search_test.go
@@ -3,11 +3,30 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
 	want := "this is just a test"
+	got, err := dictionary.Search("test")
 
+	assertNoError(t, err)
 	assertStrings(t, got, want)
 }
 
